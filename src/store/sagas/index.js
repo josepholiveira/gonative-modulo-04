514f57b3cc2d4d1c8808a509df1460335c997533
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2019-06-08 09:42:24 -0300

    Adiciona funcionalidades de PREV/NEXT no player

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -5,7 +5,7 @@ import { PodcastsTypes } from '~/store/ducks/podcasts';
 
 import { load } from './podcasts';
 import {
-  init, setPodcast, play, pause,
+  init, setPodcast, play, pause, prev, next,
 } from './player';
 
 export default function* rootSaga() {
@@ -15,5 +15,7 @@ export default function* rootSaga() {
     takeLatest(PlayerTypes.SET_PODCAST_REQUEST, setPodcast),
     takeLatest(PlayerTypes.PLAY, play),
     takeLatest(PlayerTypes.PAUSE, pause),
+    takeLatest(PlayerTypes.PREV, prev),
+    takeLatest(PlayerTypes.NEXT, next),
   ]);
 }
--- src/store/sagas/player.js
+++ src/store/sagas/player.js
@@ -1,12 +1,32 @@
-import { call, put, select } from 'redux-saga/effects';
+import {
+  call, put, select, take,
+} from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
 import TrackPlayer from 'react-native-track-player';
 
 import PlayerActions from '~/store/ducks/player';
 
+function* trackChanged() {
+  const channel = eventChannel((emitter) => {
+    const onTrackChange = TrackPlayer.addEventListener('playback-track-changed', emitter);
+
+    return () => onTrackChange.remove();
+  });
+
+  try {
+    while (true) {
+      const { nextTrack } = yield take(channel);
+
+      yield put(PlayerActions.setCurrent(nextTrack));
+    }
+  } finally {
+    channel.close();
+  }
+}
+
 export function* init() {
   yield call(TrackPlayer.setupPlayer);
 
-  TrackPlayer.addEventListener('playback-track-changed', console.tron.log);
   TrackPlayer.addEventListener('playback-state', console.tron.log);
 }
 
@@ -27,6 +47,7 @@ export function* setPodcast({ podcast, episodeId }) {
   }
 
   yield put(PlayerActions.play());
+  yield call(trackChanged);
 }
 
 export function* play() {
@@ -34,4 +55,24 @@ export function* play() {
 }
 export function* pause() {
   yield call(TrackPlayer.pause);
+}
+
+export function* prev() {
+  const player = yield select(state => state.player);
+  const currentIndex = player.podcast.tracks.findIndex(episode => episode.id === player.current);
+
+  if (player.podcast.tracks[currentIndex - 1]) {
+    yield call(TrackPlayer.skipToPrevious);
+    yield put(PlayerActions.play());
+  }
+}
+
+export function* next() {
+  const player = yield select(state => state.player);
+  const currentIndex = player.podcast.tracks.findIndex(episode => episode.id === player.current);
+
+  if (player.podcast.tracks[currentIndex + 1]) {
+    yield call(TrackPlayer.skipToNext);
+    yield put(PlayerActions.play());
+  }
 }
